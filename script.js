cccc57d2b0b2b8065fc8a1d235fbe9652918d983
Ryan Robby <147597256+RyanRyzer@users.noreply.github.com>
2024-10-18 22:37:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/** Source Code Nama dan Origin */
+/** Source Code Nama dan Origin pada penggunaan Function */
 
 function hello(name, origin = "Bandung") {
     return `Halo! Nama saya ${name}. Saya tinggal di ${origin}.`;
@@ -9,3 +9,22 @@ let indra = hello('Indra');
 
 console.log(rahmat);
 console.log(indra);
+
+
+/** Source Code Sintaksis */
+
+// TODO: buatlah variabel (konstan) bernama `currency` dan isi dengan nilai "IDR".
+const currency = "IDR";
+console.log(currency);
+// TODO: buatlah variabel bernama `value` dan isi dengan nilai 10000.
+let value = 10000;
+console.log(value);
+// TODO: tambahkan nilai di dalam variabel `value` sebesar 5000.
+value += 5000;
+console.log(value);
+/**
+ * TODO: buatlah variabel (konstan) bernama `money`,
+ * isi dengan penambahan string dari nilai `currency` + " " + `value`.
+ */
+const money = currency + " " + value;
+console.log(money);
